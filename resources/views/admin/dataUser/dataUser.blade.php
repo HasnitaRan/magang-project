cd62a68b3436aa79de4ce549b6c3ea5dcf592e1e
kartikadev11 <kartikadevianii@gmail.com>
2024-09-29 15:47:24 +0700

    make data user menu and active side bar menu

--- resources/views/admin/dataUser/dataUser.blade.php
+++ resources/views/admin/dataUser/dataUser.blade.php
@@ -1,4 +1,11 @@
-@include('layouts.main.navbar')
-@include('layouts.sidebar.admin')
+<div id="layoutSidenav_content">
+    <main>
+        <div class="px-4 container-fluid">
+            <h1 class="mt-4">Data User</h1>
+            <ol class="mb-4 breadcrumb">
+                <li class="breadcrumb-item active">SMAN 01 Tegal</li>
+            </ol>
 
-@include('layouts.main.footer')
+
+        </div>
+    </main>
